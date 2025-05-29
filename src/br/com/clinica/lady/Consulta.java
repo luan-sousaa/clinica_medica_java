49b52dc6854548df25fbe1a451dfa5736376b661
Yas <yasespigolani@gmail.com>
2025-05-28 21:59:12 -0300

    Update Consulta.java
    Comentários

--- src/br/com/clinica/lady/Consulta.java
+++ src/br/com/clinica/lady/Consulta.java
@@ -1,15 +1,19 @@
+
+// Pacote onde a classe está inserida
 package br.com.clinica.lady;
 
+// Criação da classe "Consulta" e seus atributos
 public class Consulta {
     private int id_consulta;
     private int id_medico;
     private String categoria;
     private double valor;
 
+// Método "get" utilizado para ler a variável
     public String getCategoria() {
         return categoria;
     }
-
+// Método "set" utilizado para alterar a variável
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
@@ -33,12 +37,15 @@ public class Consulta {
         this.valor = valor;
     }
 
+    // Construtor da classe criada, utilizado para inicializar os atributos
     public Consulta(String categoria, int id_consulta, int id_medico, double valor) {
         this.categoria = categoria;
         this.id_consulta = id_consulta;
         this.id_medico = id_medico;
         this.valor = valor;
     }
+
+    // Método da classe
     public void marcar_consulta(){
         System.out.println("Consulta cadastrada com sucesso!");
     }
