62c33ca173be833109b4613668659de48e92da96
Yas <yasespigolani@gmail.com>
2025-05-28 22:09:52 -0300

    Update Prontuario.java

--- src/br/com/clinica/lady/Prontuario.java
+++ src/br/com/clinica/lady/Prontuario.java
@@ -1,12 +1,16 @@
+// Pacote onde a classe está inserida
 package br.com.clinica.lady;
+
+// Importação da classe pai "Paciente" para o acesso de dados
 import br.com.clinica.lady.Paciente;
 
+// Criação da classe "Prontuario" que herda informações da classe pai
 public class Prontuario extends Paciente {
     private int id_prontuario;
     private int id_paciente;
     private String diagnostico;
 
-
+    // Construtor da classe criada, utilizado para inicializar os atributos
     public Prontuario(String cpf, String data_nascimento, String endereco, int id_paciente, String nome, String sexo, String diagnostico, int id_paciente1, int id_prontuario) {
         super(cpf, data_nascimento, endereco, id_paciente, nome, sexo);
         this.diagnostico = diagnostico;
@@ -15,11 +19,13 @@ public class Prontuario extends Paciente {
         this.getNome();
 
     }
-
+    
+// Método "get" utilizado para ler a variável
     public String getDiagnostico() {
         return diagnostico;
     }
-
+    
+// Método "set" utilizado para alterar a variável
     public void setDiagnostico(String diagnostico) {
         this.diagnostico = diagnostico;
     }
@@ -36,7 +42,7 @@ public class Prontuario extends Paciente {
 
 
 
-
+//Método da classe que apresentar as informações que estão contidas no prontuário do paciente, juntamente com o diagnóstico
     public void exibirProntuario() {
         System.out.println("\n");
         System.out.println("PRONTUARIO DO PACIENTE");
